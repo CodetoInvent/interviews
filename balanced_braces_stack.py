7eb954de5bc4f146f5d7c87eb11ded48b9e6ce0b
Rashiq <rashiq.z@gmail.com>
2017-05-27 12:50:21 +0200

    Add O(1) memory solution for balanced braces

--- balanced_braces_stack.py
+++ balanced_braces_stack.py
@@ -18,16 +18,27 @@ braces = {'}': '{', ']': '[', ')': '('}
 def validate_braces(str):
     stack = []
     for i in str:
-        if i not in braces: 
+        if i not in braces:
             stack.append(i)
             continue
-        
-        if not stack: return False
-        pop = stack.pop()
-        if braces[i] != pop: return False
+
+        if not stack or braces[i] != stack.pop():
+            return False
     return True
 
 
-for i, v in check.items(): 
+for i, v in check.items():
     assert validate_braces(i) == v
 
+def validate_braces(string):
+    balance = 0
+
+    for i in string:
+        if i == '(':
+            balance += 1
+        if i == ')':
+            balance -= 1
+        if balance < 0:
+            return False
+
+    return balance == 0
